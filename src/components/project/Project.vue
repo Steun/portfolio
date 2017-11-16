68af1c264fd2f60ef355311f2f83107e19003522
Teun Kelting <teunkelting@hotmail.com>
2017-11-17 00:04:21 +0100

    WIP on progress bar

--- src/components/project/Project.vue
+++ src/components/project/Project.vue
@@ -7,18 +7,19 @@
       <h1 class="title">{{currentProject.title}}</h1>
       <h2 class="subtitle">{{currentProject.subtitle}}</h2>
       <div class="curlywurly">
-        <svg width="310" height="62" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 60" preserveAspectRatio>
+        <svg width="310" height="62" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 60">
           <polyline fill="none" stroke="#5148FF"
                     points="7,50 50,7 100,50 150,7 200,50 250,7 300,50"/>
         </svg>
       </div>
+      <progress :value="progress" max="100" style="position:absolute;"></progress>
     </div>
 
     <div class="description-box" v-if="currentProject.description">
       <p class="description">{{currentProject.description}}</p>
     </div>
 
-    <project-info ref="project" :current-project="currentProject"></project-info>
+    <project-info :current-project="currentProject"></project-info>
 
   </div>
 
--- src/components/project/Project.vue.js
+++ src/components/project/Project.vue.js
@@ -1,3 +1,4 @@
+import router from '@/router'
 import store from '@/store'
 import ProjectInfo from '@/components/project-info/ProjectInfo'
 import { mapActions } from 'vuex'
@@ -6,7 +7,10 @@ export default {
   name: 'project',
   data() {
     return {
-      currentProject: store.state.currentProject
+      currentProject: store.state.currentProject,
+      progress: 0,
+      currentIndex: 0,
+      nextIndex: 1
     }
   },
   methods: {
@@ -19,10 +23,31 @@ export default {
         this.actionSetCurrentProject(store.getters.getProject(project))
         this.currentProject = store.state.currentProject
       }
+    },
+
+    updateIndexes() {
+      this.currentIndex = store.state.projects.findIndex(project => project.name === store.state.currentProject.name)
+      this.nextIndex = (this.currentIndex === store.state.projects.length - 1) ? 1 : this.currentIndex + 1
+    },
+
+    ticker() {
+      setInterval(() => {
+        if (this.progress < 100) {
+          this.progress += 0.2
+        } else {
+          this.updateIndexes()
+          // let currentIndex = store.state.projects.findIndex(project => project.name === store.state.currentProject.name)
+          // let nextIndex = (this.currentIndex === store.state.projects.length - 1) ? 1 : this.currentIndex + 1
+
+          router.push({ name: 'project', params: { project: store.state.projects[this.nextIndex].name } })
+        }
+      }, 10)
     }
 
   },
   mounted() {
+    // this.updateIndexes()
+    // this.ticker()
     if (this.$route.params.project) {
       this.setCurrentProject(this.$route.params.project)
     } else {
